9dcf768917484fc58583d53cefe71a8ed3e9a64b
Amboy13 <112390917+Amboy13@users.noreply.github.com>
2022-11-24 19:18:31 +0800

    Add files via upload
    first commit

---
+++ my.js
@@ -0,0 +1,31 @@
+$(document).on('click', 'a[href^="#"]', function (event) {
+  event.preventDefault();
+
+  $('html, body').animate({
+    scrollTop: $($.attr(this, 'href')).offset().top
+  }, 500);
+});
+
+$(window).scroll(function(){
+  if($(window).scrollTop()){
+    $("nav").addClass("black");
+  }
+  else{
+    $("nav").removeClass("black");
+  }
+});
+
+const handleOnMouseMove = e => {
+  const { currentTarget: target } = e;
+
+  const rect = target.getBoundingClientRect(),
+      x = e.clientX - rect.left,
+      y = e.clientY - rect.top;
+
+  target.style.setProperty("--mouse-x", '${x}px');
+  target.style.setProperty("--mouse-y", '${y}px');
+}
+for(const card of document.querySelectorAll(".card")){
+  card.onmousemove = e => handleOnMouseMove(e);
+}
+
